0fd4e6609581c388ad6aa262f8d1c51b6a637220
ehdnd_mac <taeungjang@naver.com>
2024-09-25 23:02:23 +0900

    2180; SCCC_week2; X;
    시간초과

---
+++ week2_greedy/6_2180.py
@@ -0,0 +1,24 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+fires = []
+etc = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    if b == 0:
+        continue
+    elif a == 0:
+        etc.append(b)
+    else:
+        fires.append([a, b])
+
+
+fires.sort(reverse=True, key=lambda x: (x[0] / x[1], x[0]))
+
+cnt = 0
+for fire in fires:
+    cnt += fire[0] * cnt + fire[1]
+cnt += sum(etc)
+print(cnt % 40000)
