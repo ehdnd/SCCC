c7622f5d0093fb584d7cbed94d536110f1ac209d
ehdnd_mac <taeungjang@naver.com>
2024-09-25 13:41:02 +0900

    2798; THEME12; 다른 정답들

--- BOJ_THEME/12_brute-force/1_2798.py
+++ BOJ_THEME/12_brute-force/1_2798.py
@@ -24,3 +24,17 @@ if M in sums:
     print(sums[bisect_left(sums, M)])
 else:
     print(sums[bisect_left(sums, M) - 1])
+
+# # 다른 답
+# for i in range(N - 1):
+#     for j in range(i + 1, N):
+#         for k in range(j + 1, N):
+#             if cards[i] + cards[j] + cards[k] > M:
+#                 continue
+#             else:
+#                 result = max(result, cards[i] + cards[j] + cards[k])
+# print(result)
+
+
+# # 다른 답
+# from itertools import combinations
