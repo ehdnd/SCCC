e522602feca1949d428e4b2414f939c4f3e60054
ehdnd_mac <taeungjang@naver.com>
2024-09-25 12:21:43 +0900

    2878; SCCC_week2; f
    접근은 좋았으나 분배 알고리즘 수정 필요했었다

--- week2_greedy/4_2878.py
+++ week2_greedy/4_2878.py
@@ -1,3 +1,28 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
+
+M, N = map(int, input().split())
+limits = []
+for _ in range(N):
+    limits.append(int(input()))
+limits.sort()
+
+lack = sum(limits) - M
+
+res = []
+for i in range(N):
+    evenly_give = lack // (N - i)
+    limit = limits[i]
+    if evenly_give <= limit:
+        to_give = evenly_give
+    else:
+        to_give = limit
+
+    lack -= to_give
+    res.append(to_give)
+
+cnt = 0
+for x in res:
+    cnt += x**2
+print(cnt % 2**64)
