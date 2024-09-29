9ea3cd778b3c081a58c68ae4651384ec7b94596b
ehdnd_mac <taeungjang@naver.com>
2024-09-29 14:07:59 +0900

    1436;class2;X

---
+++ solvedac/class2/1436.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+N = int(input())
+
+
+# 그냥 6이 세 개 이상만
+# 6의 개수가 3개 이상이 되면 cnt 올려서 N에 도달하면 리턴하자
+# 666부터 시작해서 1씩 증가시키자그냥
+cnt = 1
+res = 666
+
+while 1:
+    res += 1
+    if :
+        cnt += 1
+    if cnt == N:
+        break
+
+print(res)
