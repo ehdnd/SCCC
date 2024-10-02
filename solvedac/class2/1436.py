a24bee21780f4c78ef462f64ed075598ae899cc2
ehdnd_mac <taeungjang@naver.com>
2024-10-02 12:50:15 +0900

    1436; class2;

--- solvedac/class2/1436.py
+++ solvedac/class2/1436.py
@@ -1,7 +1,23 @@
+# if '666' in str(nth) 로 간단하게..
+
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
+
+
+def find6(list):
+    cnt = 0
+    for i in range(len(list)):
+        if cnt >= 3:
+            return True
+        elif list[i] == "6":
+            cnt += 1
+        else:
+            cnt = 0
+    if cnt >= 3:
+        return True
+    return False
 
 
 # 그냥 6이 세 개 이상만
@@ -11,10 +27,10 @@ cnt = 1
 res = 666
 
 while 1:
-    res += 1
-    if :
-        cnt += 1
     if cnt == N:
         break
+    res += 1
+    if find6(list(str(res))):
+        cnt += 1
 
 print(res)
