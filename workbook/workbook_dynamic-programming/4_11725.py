cad0f8f29aa4e1a4d092ccf03cef5312fb0e0448
ehdnd_desktop <taeungjang@naver.com>
2024-10-04 01:06:01 +0900

    POC: [11725] (S3) 2xn 타일링 - 문제 풀이 중

---
+++ workbook/workbook_dynamic-programming/4_11725.py
@@ -0,0 +1,14 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+N = int(input())
+
+F = [0] * 1001
+F[1] = 1
+F[2] = 2
+
+for n in range(3, N + 1):
+    F[n] = (F[n - 2] + 2) + F[n - 1]
+    F[n] %= 10007
+
+print(F[N])
