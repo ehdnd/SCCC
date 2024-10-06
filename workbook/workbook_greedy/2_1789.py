ed0a39236aeae40724f5044e511f3cf1422e876f
ehdnd_mac <taeungjang@naver.com>
2024-10-06 14:41:56 +0900

    COR: [1789] (S5) 수들의 합 - 1처럼 작은 예시도 확인하기
    생각을 간단하게 하자. 무조건 남은수가 num 범위에 있을 수 밖에 없다

--- workbook/workbook_greedy/2_1789.py
+++ workbook/workbook_greedy/2_1789.py
@@ -3,14 +3,27 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 
-num = 0
+# num = 0
+total = 0
 cnt = 0
+if N == 1:
+    cnt += 1
+
 for i in range(1, N):
-    if num > N:
+    total += i
+    # num = i
+    cnt = i
+    if total > N:
+        total -= i
+        # num -= 1
         cnt -= 1
         break
-    elif num == N:
-        break
-    num += i
-    cnt += 1
+
 print(cnt)
+
+# # 남은 수가 num 범위에 있는지
+# if N - total <= num:
+#     cnt -= 1
+#     print(N - total, num)
+# else:
+#     print(num, cnt, total, N - total)
