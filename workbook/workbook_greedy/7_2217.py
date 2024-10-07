138a693bec05cd53f79074b3535f041c17e07d34
ehdnd_mac <taeungjang@naver.com>
2024-10-08 00:31:42 +0900

    WOR: [2217] (S4) 로프 - 반례: 5 2 1 1 1 1 1 1 1

--- workbook/workbook_greedy/7_2217.py
+++ workbook/workbook_greedy/7_2217.py
@@ -2,6 +2,21 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
-ropes = []
-for _ in range(N):
-    ropes.append(int(input()))
+ropes = [0] * N
+for i in range(N):
+    ropes[i] = int(input())
+
+ropes.sort(reverse=True)
+
+max = 0
+for i in range(N - 1):
+    max_1 = ropes[i] * (i + 1)
+    max_2 = ropes[i + 1] * (i + 1 + 1)
+    if max_1 > max_2:
+        max = max_1
+        break
+
+if max == 0:
+    max = ropes[-1] * N
+
+print(max)
