bcfdf2ce9a286470c5bd1151db5f6609b8fbfdb2
ehdnd_mac <taeungjang@naver.com>
2024-10-08 17:24:45 +0900

    COR: [13305] (S3) 주유소 - 앞에서부터 가다가 싼 주유소 만나면 교체
    더 쉽게 갈 수 있는 방법 고민해보기
    문제를 이해하고 풀이 시작하기

--- workbook/workbook_greedy/9_13305.py
+++ workbook/workbook_greedy/9_13305.py
@@ -2,18 +2,14 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
-money = 0
 lengths = list(map(int, input().split()))
 prices = list(map(int, input().split()))
 
-min_price_idx = -1
-while 1:
-    total_length = sum(lengths)
-    if total_length <= 0:
-        break
-    prices = prices[:min_price_idx]
-    min_price_idx = prices.index(min(prices))
-    money += sum(prices[min_price_idx] * lengths[min_price_idx:])
-    lengths = lengths[:min_price_idx]
+money = 0
+min_price = prices[0]
+for i in range(N - 1):
+    if prices[i] < min_price:
+        min_price = prices[i]
+    money += min_price * lengths[i]
 
 print(money)
