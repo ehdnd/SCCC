cf69d7e55e7b8d6e7d165e54be250dc78b1ffce2
ehdnd_mac <taeungjang@naver.com>
2024-10-08 16:49:25 +0900

    WOR: [13305] (S3) 주유소 - 가장 저렴한 가격으로 많이 사자?
    시간초과인듯

--- workbook/workbook_greedy/9_13305.py
+++ workbook/workbook_greedy/9_13305.py
@@ -1,3 +1,19 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
+N = int(input())
+money = 0
+lengths = list(map(int, input().split()))
+prices = list(map(int, input().split()))
+
+min_price_idx = -1
+while 1:
+    total_length = sum(lengths)
+    if total_length <= 0:
+        break
+    prices = prices[:min_price_idx]
+    min_price_idx = prices.index(min(prices))
+    money += sum(prices[min_price_idx] * lengths[min_price_idx:])
+    lengths = lengths[:min_price_idx]
+
+print(money)
