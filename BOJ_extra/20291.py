a979748549cc81dd80964c0df59538d8e1902bb2
ehdnd_mac <taeungjang@naver.com>
2024-10-08 23:14:40 +0900

    COR: [20291] (S3) 파일 정리 - 요소 개수는 이분탐색으로 시간초과 해결
    딕셔너리를 이용한 더 나은 알고리즘이 존재

--- BOJ_extra/20291.py
+++ BOJ_extra/20291.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_left, bisect_right
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
@@ -7,7 +8,8 @@ arr = []
 for i in range(N):
     arr.append(input().split(".")[1])
 
+arr.sort()
 vs = sorted(list(set(arr)))
 
 for x in vs:
-    print(x, arr.count(x))
+    print(x, bisect_right(arr, x) - bisect_left(arr, x))
