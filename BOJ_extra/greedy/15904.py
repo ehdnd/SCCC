e6f9df91e07fdf3adf18fb4cf290c7c4a3a3a04b
ehdnd_mac <taeungjang@naver.com>
2024-10-10 00:09:53 +0900

    REF: [15904] (S5) UCPC는 무엇의 약자일까? - 알고리즘 개선

--- BOJ_extra/greedy/15904.py
+++ BOJ_extra/greedy/15904.py
@@ -1,25 +1,13 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
-strings = list(input().split())
 
-res = []
-for string in strings:
-    for char in string:
-        if char.isupper():
-            res.append(char)
-
-U, C, P = False, False, False
-for char in res:
-    if char == "U" and not U:
-        U = True
-    elif char == "C" and U and not C:
-        C = True
-    elif char == "P" and C and not P:
-        P = True
-        C = False
-
-if U and C and P:
-    print("I love UCPC")
+i = 0
+for char in input():
+    if char == "UCPC"[i]:
+        i += 1
+    if i >= 4:
+        print("I love UCPC")
+        break
 else:
     print("I hate UCPC")
