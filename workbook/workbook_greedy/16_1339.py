adbcb61ccd9bacd6a87b3b61ca1c8589b0bdd6f6
ehdnd_mac <taeungjang@naver.com>
2024-10-10 17:59:18 +0900

    POC: [1339] (G4) 단어 수학 - 문제 풀이 중

--- workbook/workbook_greedy/16_1339.py
+++ workbook/workbook_greedy/16_1339.py
@@ -4,4 +4,8 @@ input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 chars = []
 for _ in range(N):
-    chars.append(input())
+    char = reversed(input())
+    chars.append(char)
+
+# 10A > A + B 려면 9A > B 무조건 성립하네.. 그냥 자릿수 큰놈에 큰수주자
+# [문자 + 자릿수] MAX 자릿수 순으로 할당 10^자릿수로 계산
