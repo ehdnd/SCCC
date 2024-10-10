2805217b52389b0b47f5d61251a849695056dbab
ehdnd_mac <taeungjang@naver.com>
2024-10-10 15:46:50 +0900

    IMP: [1699] (S2) 제곱수의 합 - 12는 4 4 4로 나누어야하는데,,
    12 9 1 1 1 보다는 12 4 4 4 가 더 작다.. 어떻게 구현할까?

--- workbook/workbook_dynamic-programming/6_1699.py
+++ workbook/workbook_dynamic-programming/6_1699.py
@@ -5,22 +5,6 @@ N = int(input())
 
 F = [0] * (N + 1)
 
-square = []
 for i in range(1, N + 1):
-    if i**2 > N:
-        break
-    F[i**2] = 1
-    square.append(i**2)
-
-for i in range(1, N + 1):
-    if F[i] == 1:
-        continue
-    idx = 0
-    for j in range(len(square)):
-        if square[j] < i:
-            idx = j
-        else:
-            break
-    F[i] = F[i - square[idx]] + 1
-
-print(F[N])
+    if (i ** (1 / 2)).is_integer():
+        F[i] = 0
