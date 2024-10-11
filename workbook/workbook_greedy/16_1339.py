e5731c7b7ee3928852c257f74e048457e6770d12
ehdnd_desktop <taeungjang@naver.com>
2024-10-11 15:07:59 +0900

    COR: [1339] (G4) 단어 수학 - (10 ** 자릿수) 합이 가장 큰 순서대로 할당
    구하는게 무엇인지 확인하고 그에 맞는 풀이를 진행하자
    할당하는 순서를 정할 때 완전탐색은 불가능

--- workbook/workbook_greedy/16_1339.py
+++ workbook/workbook_greedy/16_1339.py
@@ -4,8 +4,26 @@ input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 chars = []
 for _ in range(N):
-    char = reversed(input())
+    char = input()[::-1]
     chars.append(char)
 
 # 10A > A + B 려면 9A > B 무조건 성립하네.. 그냥 자릿수 큰놈에 큰수주자
 # [문자 + 자릿수] MAX 자릿수 순으로 할당 10^자릿수로 계산
+
+dic = {}
+for char in chars:
+    for i in range(len(char)):
+        if char[i] not in dic:
+            dic[char[i]] = 10**i
+        else:
+            dic[char[i]] += 10**i
+
+arr = sorted(dic.items(), reverse=True, key=lambda x: x[1])
+
+k = 9
+res = 0
+for i in range(len(arr)):
+    res += k * arr[i][1]
+    k -= 1
+
+print(res)
