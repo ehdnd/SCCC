62d485d3a6704fdd46d1a8a628d209cb44c494e0
ehdnd_desktop <taeungjang@naver.com>
2024-10-11 20:19:33 +0900

    COR: [9095] (S3) 1, 2, 3 만들기 - 문제를 잘 읽자

--- solvedac/class3/9095.py
+++ solvedac/class3/9095.py
@@ -7,14 +7,12 @@ for i in range(N):
     nums.append(int(input()))
 max_num = max(nums)
 F = [0 for i in range(max_num + 1)]
-F[2] = 1
-F[3] = 3
+F[1] = 1
+F[2] = 2
+F[3] = 4
 
 for i in range(4, max_num + 1):
-    for j in range(i - 3, i):
-        F[i] += F[j]
-        if i - j <= 3 and j < 4:
-            F[i] += 1
+    F[i] = F[i - 1] + F[i - 2] + F[i - 3]
 
 for num in nums:
     print(F[num])
