5c4701bc215f3f8d9956875d8603fa95f4fee145
ehdnd_mac <taeungjang@naver.com>
2024-10-12 16:12:30 +0900

    WRG: [1912] (S2) 연속합 - 시간초과

--- BOJ_THEME/23_dynamic-programming/5_1912.py
+++ BOJ_THEME/23_dynamic-programming/5_1912.py
@@ -3,13 +3,14 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 arr = list(map(int, input().split()))
-dp = [[0 for j in range(N + 1)] for i in range(N + 1)]
 max = -1001
 
-for start in range(1, N + 1):
-    for end in range(start, N + 1):
-        dp[start][end] = dp[start][end - 1] + arr[end - 1]
-        if dp[start][end] > max:
-            max = dp[start][end]
+for i in range(N):
+    dp = [0] * (N - i)
+    dp[0] = arr[i]
+    for j in range(N - i):
+        dp[j] = dp[j - 1] + arr[i + j]
+        if dp[j] > max:
+            max = dp[j]
 
 print(max)
