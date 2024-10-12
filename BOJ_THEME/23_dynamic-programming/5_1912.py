a87169d7538f0490327b02355ac0ba003ecdc84c
ehdnd_mac <taeungjang@naver.com>
2024-10-12 18:44:33 +0900

    FAIL: [1912] (S2) 연속합 - Kadane's Algorithm
    연속된 부분합을 구하는 데 있어서 유리한 알고리즘
    DP를 기반으로 작성
    
    [ kadane's algorithm ]
    [-2, 2, 4, -11, 6] 에서
    -11 에서 다시 시작할 지
    globalMax는 아니지만 이전 subarrayh와 이어서 진행할 지 결정
    => local_max + current_num 과 current_num 비교
    결국 이전 subarray 중 최대인 값을 가지는 것과 현재 숫자를 비교

--- BOJ_THEME/23_dynamic-programming/5_1912.py
+++ BOJ_THEME/23_dynamic-programming/5_1912.py
@@ -3,14 +3,25 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
 arr = list(map(int, input().split()))
-max = -1001
 
-for i in range(N):
-    dp = [0] * (N - i)
-    dp[0] = arr[i]
-    for j in range(N - i):
-        dp[j] = dp[j - 1] + arr[i + j]
-        if dp[j] > max:
-            max = dp[j]
+# kadane's algorithm
+# [-2, 2, 4, -11, 6] 에서
+# -11 에서 다시 시작할 지
+# globalMax는 아니지만 이전 subarrayh와 이어서 진행할 지 결정
+# => local_max + current_num 과 current_num 비교
+# 결국 이전 subarray 중 최대인 값을 가지는 것과 현재 숫자를 비교
 
-print(max)
+sub_arr_max = arr[0]
+global_max = sub_arr_max
+
+for i in range(1, N):
+    current_num = arr[i]
+
+    # 더한 값은 sub arr 이어가기
+    # current_num 은 sub arr 새로 시작하기
+    sub_arr_max = max(sub_arr_max + current_num, current_num)
+
+    # global_max 갱신하기
+    global_max = max(global_max, sub_arr_max)
+
+print(global_max)
