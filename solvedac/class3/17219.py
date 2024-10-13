16b40f1ae4707639bf6acb266ea779761bf60185
ehdnd_mac <taeungjang@naver.com>
2024-10-13 15:22:05 +0900

    COR: [17219] (S4) 비밀번호 찾기 - 파이썬 짱

---
+++ solvedac/class3/17219.py
@@ -0,0 +1,12 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+N, M = map(int, input().split())
+
+dic = {}
+for _ in range(N):
+    k, v = input().split()
+    dic[k] = v
+
+for _ in range(M):
+    print(dic[input()])
