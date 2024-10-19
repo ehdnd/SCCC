b2c284b206fce888d3a634ef7da8a2cbca1a93cb
ehdnd_desktop <taeungjang@naver.com>
2024-10-20 00:07:49 +0900

    COR: [1715] (G4) 카드 정렬하기 - 매번 가장 작은 카드뭉치 두개 더하기 / 힙큐
    우선순위 큐, 힙큐를 사용하면 정렬이 필요없네..

--- workbook/workbook_greedy/15_1715.py
+++ workbook/workbook_greedy/15_1715.py
@@ -1,7 +1,18 @@
 import sys
+import heapq
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
-cards = [0] * N
-for i in range(N):
-    cards[i] = int(input())
+cards = []
+for _ in range(N):
+    heapq.heappush(cards, int(input()))
+
+res = 0
+while 1:
+    if len(cards) <= 1:
+        break
+    sum = heapq.heappop(cards) + heapq.heappop(cards)
+    res += sum
+    heapq.heappush(cards, sum)
+
+print(res)
