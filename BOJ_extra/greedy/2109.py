23ef46703d4799de774da0ea85d64224c4982312
ehdnd_mac <taeungjang@naver.com>
2024-10-22 18:25:43 +0900

    COR: [2109] (G3) 순회강연 - n = 0 고려하기 / pop() O(1)이래
    while 돌면서 앞에서 pop 보다는 뒤에서 pop이 안정적이다
    while 조건1 and 조건2 라면 조건1 만족 안할경우 조건2 검사안함

--- BOJ_extra/greedy/2109.py
+++ BOJ_extra/greedy/2109.py
@@ -7,14 +7,13 @@ lectures = []
 for _ in range(N):
     p, d = map(int, input().split())
     lectures.append((d, p))
-lectures.sort(reverse=True, key=lambda x: (x[0], x[1]))
+lectures.sort(key=lambda x: (x[0], x[1]))
 
 res = 0
 temp = []
-max_date = lectures[0][0]
-for i in range(max_date, 0, -1):
-    while lectures and lectures[0][0] >= i:
-        d, p = lectures.pop(0)
+for i in range(N, 0, -1):
+    while lectures and lectures[-1][0] >= i:
+        d, p = lectures.pop()
         heapq.heappush(temp, -p)
 
     if temp:
