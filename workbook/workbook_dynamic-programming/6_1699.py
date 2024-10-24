6e6795edb17b02a8532117acee8f5731c86fbcb7
ehdnd_desktop <taeungjang@naver.com>
2024-10-25 00:24:57 +0900

    WRG: [1699] (S2) 제곱수의 합 - 반례 32..

--- workbook/workbook_dynamic-programming/6_1699.py
+++ workbook/workbook_dynamic-programming/6_1699.py
@@ -2,9 +2,13 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 N = int(input())
-
 F = [0] * (N + 1)
 
 for i in range(1, N + 1):
     if (i ** (1 / 2)).is_integer():
-        F[i] = 0
+        F[i] = 1
+        sq = i
+    else:
+        F[i] = min(F[i - 1] + 1, F[sq] + F[i - sq])
+
+print(F[N])
