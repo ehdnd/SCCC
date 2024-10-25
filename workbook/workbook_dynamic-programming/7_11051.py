eb663c435c623a5ccc9fdceab69eda234e64390b
ehdnd_mac <taeungjang@naver.com>
2024-10-25 16:43:18 +0900

    COR: [11051] (S2) 이항 계수 2 - nCk 중 n을 기준으로 dp 작성
    파스칼의 삼각형을 활용했어야.. nCk = n-1Ck-1 + n-1Ck

--- workbook/workbook_dynamic-programming/7_11051.py
+++ workbook/workbook_dynamic-programming/7_11051.py
@@ -1,1 +1,18 @@
-# 이항계수
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+N, K = map(int, input().split())
+
+
+def jud(n, k):
+    dp = [0] * 1001
+    if n == k:
+        return 1
+
+    dp[k] = 1
+    for i in range(k + 1, n + 1):
+        dp[i] = (dp[i - 1] * i) // (i - k)
+    return dp[n]
+
+
+print(jud(N, K) % 10007)
