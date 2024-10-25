5d04e6e2f88976d9ac59af2303e374348ddf9b0a
ehdnd_mac <taeungjang@naver.com>
2024-10-25 21:57:55 +0900

    FAIL: [1699] (S2) 제곱수의 합 - dp[n] = n 만들기 위한 최소 제곱수 개수
    dp[n] = min(1+dp[n-a]) 로 보아서 (a는 n이하인 제곱수)
    n-a를 제곱수의 합으로 나타냈을 때 최소 개수를 활용하
    n을 계산할 때 a의 개수는 최대 sqrt(n) 개 이므로 O(n sqrt n) 에 계산가

--- workbook/workbook_dynamic-programming/6_1699.py
+++ workbook/workbook_dynamic-programming/6_1699.py
@@ -1,20 +1,19 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
+
+
+def jud(N):
+    dp = [100001] * (N + 1)
+    dp[0] = 0
+
+    for s in range(1, N + 1):
+        e = 1
+        while e * e <= s:
+            dp[s] = min(dp[s], dp[s - e * e] + 1)
+            e += 1
+    return dp[N]
+
+
 N = int(input())
-F = [0] * (N + 1)
-
-for i in range(1, N + 1):
-    if (i ** (1 / 2)).is_integer():
-        F[i] = 1
-        sq = i
-    else:
-        mini = 1000001
-        for j in range(1, sq + 1):
-            temp = F[j] + F[i - j]
-            if temp < mini:
-                mini = temp
-
-        F[i] = min(F[i - 1] + 1, mini)
-
-print(F[N])
+print(jud(N))
