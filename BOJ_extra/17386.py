685b149cbb13163170d4ea589eff176e51c76c48
ehdnd_desktop <taeungjang@naver.com>
2024-10-27 16:38:58 +0900

    REF: [17386] (G3) 선분 교차 1 - ccw * ccw < 0 이면 교차구나

--- BOJ_extra/17386.py
+++ BOJ_extra/17386.py
@@ -13,9 +13,11 @@ def ccw(x1, y1, x2, y2, x3, y3):
 def jud(L1, L2):
     x1, y1, x2, y2 = L1
     x3, y3, x4, y4 = L2
-    if ccw(x1, y1, x2, y2, x3, y3) != ccw(x1, y1, x2, y2, x4, y4) and ccw(
-        x1, y1, x3, y3, x4, y4
-    ) != ccw(x2, y2, x3, y3, x4, y4):
+    ccw_123 = ccw(x1, y1, x2, y2, x3, y3)
+    ccw_124 = ccw(x1, y1, x2, y2, x4, y4)
+    ccw_341 = ccw(x1, y1, x3, y3, x4, y4)
+    ccw_342 = ccw(x2, y2, x3, y3, x4, y4)
+    if ccw_123 * ccw_124 < 0 and ccw_341 * ccw_342 < 0:
         return 1
     return 0
 
