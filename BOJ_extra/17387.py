802b913dee1b8380422936a826eab2007f312425
ehdnd_desktop <taeungjang@naver.com>
2024-10-27 18:42:57 +0900

    WRG: [17387] (G2) 선분 교차 2 - 반례 추가
    0 1 0 2
    0 3 0 4
    기울기가 무한대라면 y값도 비교 필요

--- BOJ_extra/17387.py
+++ BOJ_extra/17387.py
@@ -10,10 +10,21 @@ def ccw(x1, y1, x2, y2, x3, y3):
     elif res < 0:
         return -1
     else:
-        if (x1 <= x3 <= x2 or x2 <= x3 <= x1) and (y1 <= y3 <= y2 or y2 <= y3 <= y1):
-            return 2
-        else:
-            return 0
+        return 0
+
+
+def isMiddle(L1, L2):
+    x1, y1, x2, y2 = L1
+    x3, y3, x4, y4 = L2
+    if (
+        min(x1, x2) <= max(x3, x4)
+        and min(x3, x4) <= max(x1, x2)
+        and min(y1, y2) <= max(y3, y4)
+        and min(y3, y4) <= max(y1, y2)
+    ):
+        return True
+    else:
+        return False
 
 
 def jud(L1, L2):
@@ -23,13 +34,15 @@ def jud(L1, L2):
     ccw_124 = ccw(x1, y1, x2, y2, x4, y4)
     ccw_341 = ccw(x1, y1, x3, y3, x4, y4)
     ccw_342 = ccw(x2, y2, x3, y3, x4, y4)
-    if ccw_123 == 2 or ccw_124 == 2 or ccw_341 == 2 or ccw_342 == 2:
-        return 1
-    elif ccw_123 * ccw_124 == 0 or ccw_341 * ccw_342 == 0:
-        return 0
-    else:
-        if ccw_123 * ccw_124 < 0 and ccw_341 * ccw_342 < 0:
+
+    # if ccw_123 == 0 and ccw_124 == 0 and ccw_341 == 0 and ccw_342 == 0:
+    if ccw_123 * ccw_124 == 0 and ccw_341 * ccw_342 == 0:
+        if isMiddle(L1, L2):
             return 1
+
+    if ccw_123 * ccw_124 <= 0 and ccw_341 * ccw_342 <= 0:
+        return 1
+
     return 0
 
 
