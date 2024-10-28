0ae1116ea2e8d3a12ea5a62e9662a64fac8185c8
ehdnd_mac <taeungjang@naver.com>
2024-10-29 00:03:10 +0900

    REF: [2164] (S4) 카드2 - 덱 생성 방법과 pop만 신경는 문제

--- weekly_lecture/3_stack-queue-deque/2164.py
+++ weekly_lecture/3_stack-queue-deque/2164.py
@@ -5,17 +5,12 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def jud(N):
-    deq = deque()
-    for i in range(1, N + 1):
-        deq.append(i)
-    if N == 1:
-        return 1
-    while 1:
+    deq = deque(i for i in range(1, N + 1))
+    while len(deq) > 1:
         deq.popleft()
-        if len(deq) == 1:
-            return deq.popleft()
         a = deq.popleft()
         deq.append(a)
+    return deq[0]
 
 
 N = int(input())
