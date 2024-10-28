2c9927018e23c41e2e6fa3dbc9f18811e94cf03a
ehdnd_mac <taeungjang@naver.com>
2024-10-28 16:56:56 +0900

    POC: [2110] (G4) 공유기 설치

---
+++ weekly_lecture/1_binary-search/5_2110.py
@@ -0,0 +1,15 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def isPossible():
+    pass
+
+
+def jud():
+    pass
+
+
+N, C = map(int, input().split())
+houses = sorted([int(input()) for _ in range(N)])
