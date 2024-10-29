2d2c9b3fb101c114a84bbd5ce8e382109102255f
ehdnd_mac <taeungjang@naver.com>
2024-10-29 23:15:39 +0900

    REF: [1966] (S3) 프린터 큐- enumerate

--- weekly_lecture/3_stack-queue-deque/1966.py
+++ weekly_lecture/3_stack-queue-deque/1966.py
@@ -7,9 +7,9 @@ input = lambda: sys.stdin.readline().rstrip()
 def jud(M, deq):
     cnt = 1
     while 1:
-        current = max(deq)[0]
-        if deq[0][0] == current:
-            if deq[0][1] == M:
+        current = max(deq, key=lambda x: x[1])[1]
+        if deq[0][1] == current:
+            if deq[0][0] == M:
                 return cnt
             deq.popleft()
             cnt += 1
@@ -20,8 +20,5 @@ def jud(M, deq):
 T = int(input())
 for i in range(T):
     N, M = map(int, input().split())
-    arr = list(map(int, input().split()))
-    deq = deque()
-    for j in range(N):
-        deq.append((arr[j], j))
+    deq = deque(enumerate(list(map(int, input().split()))))
     print(jud(M, deq))
