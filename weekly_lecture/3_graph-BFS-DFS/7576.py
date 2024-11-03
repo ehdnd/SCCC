169f68255cc3f60c90b1352e9fe833d8afaca1cd
ehdnd_desktop <taeungjang@naver.com>
2024-11-03 19:07:01 +0900

    REF: [7576] (G5) 토마토 - 입력 개선, visit 배열 선언 불필요
    2차원 배열 max 값은 max(map(max, G))로 받아야한다

--- weekly_lecture/3_graph-BFS-DFS/7576.py
+++ weekly_lecture/3_graph-BFS-DFS/7576.py
@@ -4,8 +4,12 @@ from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def BFS(tomato, visit):
-    q = deque(tomato)
+def BFS(G, N, M):
+    q = deque()
+    for i in range(N):
+        for j in range(M):
+            if G[i][j] == 1:
+                q.append([i, j])
     dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
     while q:
         x, y = q.popleft()
@@ -16,37 +20,22 @@ def BFS(tomato, visit):
                 or nx > N - 1
                 or ny < 0
                 or ny > M - 1
-                or visit[nx][ny]
+                or G[nx][ny] >= 1
                 or G[nx][ny] == -1
             ):
                 continue
             q.append([nx, ny])
-            visit[nx][ny] = visit[x][y] + 1
+            G[nx][ny] = G[x][y] + 1
 
     for i in range(N):
         for j in range(M):
-            if visit[i][j] == 0 and G[i][j] == 0:
+            if G[i][j] == 0:
                 return -1
 
-    return max(map(max, visit)) - 1
+    return max(map(max, G)) - 1
 
 
 M, N = map(int, input().split())
-G = [[-1] * 1010 for _ in range(1010)]
-visit = [[0] * 1010 for _ in range(1010)]
-for i in range(N):
-    G[i] = list(map(int, input().split()))
+G = [list(map(int, input().split())) for _ in range(N)]
 
-tomato = []
-for i in range(N):
-    for j in range(M):
-        if G[i][j] == 1:
-            tomato.append([i, j])
-            visit[i][j] = 1
-
-print(BFS(tomato, visit))
-
-# for i in range(N):
-#     for j in range(M):
-#         print(visit[i][j], end=" ")
-#     print()
+print(BFS(G, N, M))
