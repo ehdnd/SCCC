8836e01fce629a2895852ff85e9b45bb17ba09e1
ehdnd_desktop <taeungjang@naver.com>
2024-11-03 23:10:52 +0900

    REF: [4179] (G3) 불! - 간단한 코드 수정.. 큐 두개 쓰는건 나중에 해보자..

--- weekly_lecture/3_graph-BFS-DFS/4179.py
+++ weekly_lecture/3_graph-BFS-DFS/4179.py
@@ -5,54 +5,42 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def BFS(N, M, G, V):
-    temp = []
+    q = deque()
     dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
     for i in range(N):
         for j in range(M):
             if G[i][j] == "F":
-                temp.append([i, j, 0])
+                q.appendleft([i, j, 0])
                 V[i][j][0] = 1
             elif G[i][j] == "J":
-                temp.append([i, j, 1])
+                q.append([i, j, 1])
                 V[i][j][1] = 1
-    q = deque(sorted(temp, key=lambda x: x[2]))
 
     while q:
         x, y, w = q.popleft()
 
-        if w == 0:
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if (
-                    nx < 0
-                    or nx > N - 1
-                    or ny < 0
-                    or ny > M - 1
-                    or V[nx][ny][w]
-                    or not G[nx][ny] == "."
-                ):
+        if w == 1 and (x == 0 or x == N - 1 or y == 0 or y == M - 1):
+            return V[x][y][1]
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if (
+                nx < 0
+                or nx > N - 1
+                or ny < 0
+                or ny > M - 1
+                or V[nx][ny][0]
+                or not G[nx][ny] == "."
+            ):
+                continue
+            if w == 1:
+                if V[nx][ny][1]:
                     continue
-                q.append([nx, ny, w])
+                V[nx][ny][w] = V[x][y][w] + 1
+            else:
                 V[nx][ny][w] = 1
 
-        if w == 1:
-            if x == 0 or x == N - 1 or y == 0 or y == M - 1:
-                return V[x][y][1]
-
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if (
-                    nx < 0
-                    or nx > N - 1
-                    or ny < 0
-                    or ny > M - 1
-                    or V[nx][ny][0]
-                    or V[nx][ny][1]
-                    or not G[nx][ny] == "."
-                ):
-                    continue
-                q.append([nx, ny, w])
-                V[nx][ny][w] = V[x][y][w] + 1
+            q.append([nx, ny, w])
 
     return "IMPOSSIBLE"
 
