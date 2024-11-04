f729cd59cbeb0fe86ff096f9c25eed0b65b1f54e
ehdnd_mac <taeungjang@naver.com>
2024-11-04 16:01:01 +0900

    REF: [15649] (S3) N과 M (1) - 재귀 특성상 res 하나만 사용가능

--- weekly_lecture/3_graph-BFS-DFS/15649.py
+++ weekly_lecture/3_graph-BFS-DFS/15649.py
@@ -4,20 +4,20 @@ from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def DFS(arr, length):
-    if length == M:
-        print(*arr)
+def DFS(res):
+    if len(res) == M:
+        print(*res)
         return
 
     for i in range(1, N + 1):
-        if i in arr:
+        if i in res:
             continue
 
-        arr.append(i)
-        DFS(arr, length + 1)
-        arr.pop()
+        res.append(i)
+        DFS(res)
+        res.pop()
 
 
 N, M = map(int, input().split())
-for i in range(1, N + 1):
-    DFS([i], 1)
+res = []
+DFS(res)
