1688210fc1e012a774f732c1066db9308802879d
ehdnd_desktop <taeungjang@naver.com>
2024-11-06 00:01:25 +0900

    FAIL: [18110] (S4) solved.ac - 사사오입 round 의 파이썬
    반올림 연산을 저렇게도 하는구나
    반올림 함수를 구현하자

--- solvedac/class2/18110.py
+++ solvedac/class2/18110.py
@@ -3,16 +3,24 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
+def round(N):
+    temp = N * 0.15
+    if temp - int(temp) < 0.5:
+        return int(temp)
+    else:
+        return int(temp) + 1
+
+
 def jud():
     if N == 0:
         return 0
 
-    k = round(N * 0.15)
+    k = round(N)
     res = arr[k : N - k]
     s = sum(res)
     b = len(res)
 
-    return round(s / b)
+    return (s + b // 2) // b
 
 
 N = int(input())
