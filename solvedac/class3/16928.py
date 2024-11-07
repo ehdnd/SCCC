8e05437366642c895056fa5daea4764610d3af2d
ehdnd_mac <taeungjang@naver.com>
2024-11-07 10:08:19 +0900

    WRG: [16928] (G5) 뱀과 사다리 게임
    사다리 타고간게 cnt 낮음에도 다른 주사위가 이미 방문처리하면 문제발생

---
+++ solvedac/class3/16928.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def bfs():
+    q = deque()
+    q.append(1)
+    V[1] = 1
+
+    while q:
+        v = q.popleft()
+        if v == 100:
+            return V[v] - 1
+
+        for i in range(1, 7):
+            nv = v + i
+            if nv > 100 or V[nv]:
+                continue
+            V[nv] = V[v] + 1
+
+            if L[nv]:
+                nv = L[nv]
+            q.append(nv)
+            V[nv] = V[v] + 1
+
+
+N, M = map(int, input().split())
+V = [0] * 101
+L = [0] * 101
+for _ in range(N + M):
+    x, y = map(int, input().split())
+    L[x] = y
+
+print(bfs())
