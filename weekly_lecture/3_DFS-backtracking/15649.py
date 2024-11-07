1d23414409c496c673690278740213348b9fad23
ehdnd_mac <taeungjang@naver.com>
2024-11-07 23:26:02 +0900

    FAIL: [15649] (S3) N과 M (1) - 백트래킹

---
+++ weekly_lecture/3_DFS-backtracking/15649.py
@@ -0,0 +1,22 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def DFS(res):
+    if len(res) == M:
+        print(*res)
+        return
+
+    for i in range(1, N + 1):
+        if i in res:
+            continue
+
+        res.append(i)
+        DFS(res)
+        res.pop()
+
+
+N, M = map(int, input().split())
+res = []
+DFS(res)
