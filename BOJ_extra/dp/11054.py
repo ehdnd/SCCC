ad7b7566198e219d18ec0fe27f9bea9c0e363c8e
ehdnd_mac <taeungjang@naver.com>
2024-11-18 21:29:34 +0900

    COR: [11054] (G4) 가장 긴 바이토닉 부분 수열 - 1, 2, 3 도 바이토닉임

--- BOJ_extra/dp/11054.py
+++ BOJ_extra/dp/11054.py
@@ -4,8 +4,7 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def jud():
-    # [수열 길이, 증가 감소 여부]
-    # [증가중인 수열, 감소중인 수열]
+    # [증가하는 수열 길이, 감소하는 수열 길이]
     dp = [[1, 1] for _ in range(1000)]
     for i in range(N):
         for j in range(i):
@@ -14,45 +13,19 @@ def jud():
 
             # 증가중
             if dp[i][0]:
+                # 계속 증가
                 if nums[j] < nums[i]:
                     dp[i][0] = max(dp[i][0], dp[j][0] + 1)
+                # 이제 감소
                 else:
-                    # dp[i][0] = 0
                     dp[i][1] = max(dp[i][1], dp[j][0] + 1)
 
             # 감소중
-            if dp[i][1]:
-                if nums[j] > nums[i]:
-                    dp[i][1] = max(dp[i][1], dp[j][1] + 1)
-
-    return max(dp, key=lambda x: x[1])[1]
-
-
-# def jud():
-#     # [수열 길이, 증가 감소 여부]
-#     dp = [[1, 0] for _ in range(1000)]
-#     for i in range(N):
-#         for j in range(i):
-#             if nums[i] == nums[j]:
-#                 continue
-
-#             # 0 이거나 증가중
-#             if dp[j][1] >= 0:
-#                 dp[i][0] = max(dp[i][0], dp[j][0] + 1)
-#                 dp[i][1] = 1 if nums[j] < nums[i] else -1
-
-#             # 0 이거나 감소중
-#             if dp[i][1] <= 0 and nums[j] > nums[i]:
-#                 dp[i][0] = max(dp[i][0], dp[j][0] + 1)
-#                 dp[i][1] = -1
-
-#     print(dp[:N])
-#     res = 0
-#     for a in dp:
-#         if a[1] <= 0:
-#             res = max(res, a[0])
-
-#     return res
+            if dp[i][1] and nums[j] > nums[i]:
+                # 계속 감소해야지
+                dp[i][1] = max(dp[i][1], dp[j][1] + 1)
+    # [5, 4, 3, 2, 1] 도 바이토닉 수열
+    return max(map(max, dp))
 
 
 N = int(input())
