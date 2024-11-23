3760360e8e474de303189fd368977a3e2996cff1
ehdnd_mac <taeungjang@naver.com>
2024-11-23 14:29:12 +0900

    WRG: [31427] (P5) 신촌 도로망 관리와 쿼리 - 시간초과.. 왜?

--- weekly_lecture/6_MST/31427.py
+++ weekly_lecture/6_MST/31427.py
@@ -4,60 +4,52 @@ from itertools import permutations
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def Find(S, a):
+def Find(a):
     if S[a] < 0:
         return a
-    S[a] = Find(S, S[a])
+    S[a] = Find(S[a])
     return S[a]
 
 
-def Union(S, a, b):
-    a = Find(S, a)
-    b = Find(S, b)
+def Union(a, b):
+    a, b = Find(a), Find(b)
     if a == b:
         return 0
+    if S[a] > S[b]:
+        a, b = b, a
     S[a] += S[b]
     S[b] = a
     return 1
 
 
-def mst(Temp, G):
-    d = dict(zip(["A", "B", "C", "D", "E"], Temp))
-    G.sort(key=lambda x: d[x[-1]])
-    S = [-1] * (N + 1)
-    res = dict.fromkeys(["A", "B", "C", "D", "E"], 0)
-    for a, b, w in G:
-        if Union(S, int(a), int(b)):
-            res[w] += 1
-        if -S[Find(S, int(a))] == N:
-            break
-    return res
-
-
-def generate_cases(G):
-    Cases = {}
-    for Temp in permutations([1, 2, 3, 4, 5]):  # 1~5 중복 순열
-        case = mst(Temp, G)
-        Cases["".join(map(str, Temp))] = case
-    return Cases
-
-
 N, M, Q = map(int, input().split())
 G = [list(input().split()) for _ in range(M)]
+School = ["A", "B", "C", "D", "E"]
 
-Cases = generate_cases(G)
+Cases = [c for c in permutations([1, 2, 3, 4, 5])]
+Res_cases = {}
+
+for case in Cases:
+    school_to_w = dict(zip(School, case))
+    G.sort(key=lambda x: school_to_w[x[-1]])
+    S = [-1] * (N + 1)
+    res = dict(zip(School, [0, 0, 0, 0, 0]))
+    for a, b, w in G:
+        if Union(int(a), int(b)):
+            res[w] += 1
+        if -S[Find(int(a))] == N:
+            break
+
+    Res_cases[case] = res
 
 for _ in range(Q):
     query = list(map(int, input().split()))
-    asdf = list(zip([1, 2, 3, 4, 5], query))
-    asdf.sort(key=lambda x: x[-1])
-    tttt = list(enumerate(asdf))
-    tttt.sort(key=lambda x: x[1][0])
-    final = ""
-    for i in range(5):
-        final += str(tttt[i][0] + 1)
-
-    final_case = Cases[final]
+    q = list(zip(query, [1, 2, 3, 4, 5]))
+    q.sort()
+    qq = list(enumerate(q))
+    qq.sort(key=lambda x: x[1][1])
+    query_key = tuple([qq[i][0] + 1 for i in range(5)])
+    final_case = Res_cases[query_key]
 
     res = 0
     school = ["A", "B", "C", "D", "E"]
