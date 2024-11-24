ebd7be4e9d434397e34983bacb79f0cc5795413f
ehdnd_desktop <taeungjang@naver.com>
2024-11-24 22:50:18 +0900

    COR: [9373] (P2) 복도 뚫기 - 0 이하면 유니온파인드 미리 + 종료 조건 수정
    벽과 벽이 연결되면 종료되어야 함.
    힙큐 + 거리가 0 이하라면 연결되어있으므로 간선 생성말고 유니온

--- weekly_lecture/6_MST/9373.py
+++ weekly_lecture/6_MST/9373.py
@@ -1,5 +1,6 @@
 import sys
 from math import sqrt
+import heapq
 
 input = lambda: sys.stdin.readline().rstrip()
 
@@ -26,14 +27,21 @@ def jud():
     Lines = []
 
     # 벽 - 벽
-    Lines.append([W, 0, N + 1])
+    heapq.heappush(Lines, [W, 0, N + 1])
     # 벽1 / 벽2 - 원
     for i in range(1, N + 1):
         x, y, r = Sensors[i]
         w1 = x - r
         w2 = W - x - r
-        Lines.append([w1, 0, i]) if w1 > 0 else Lines.append([0, 0, i])
-        Lines.append([w2, i, N + 1]) if w2 > 0 else Lines.append([0, i, N + 1])
+        if w1 > 0:
+            heapq.heappush(Lines, [w1, 0, i])
+        else:
+            Union(0, i)
+        if w2 > 0:
+            heapq.heappush(Lines, [w2, i, N + 1])
+        else:
+            Union(i, N + 1)
+
     # 원 - 원
     for i in range(1, N + 1):
         x1, y1, r1 = Sensors[i]
@@ -42,14 +50,22 @@ def jud():
                 continue
             x2, y2, r2 = Sensors[j]
             w = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) - r1 - r2
-            Lines.append([w, i, j]) if w > 0 else Lines.append([0, i, j])
-    # 간선 정렬
-    Lines.sort()
+            if w > 0:
+                heapq.heappush(Lines, [w, i, j])
+            else:
+                Union(i, j)
+
+    if Find(0) == Find(N + 1):
+        return 0
 
     # 유니온파인드
-    for w, a, b in Lines:
-        Union(a, b)
-        if -S[Find(a)] == N + 2:
+    res = 0
+    while Lines:
+        w, a, b = heapq.heappop(Lines)
+        # for w, a, b in Lines:
+        if Union(a, b):
+            res = w
+        if Find(0) == Find(N + 1):
             break
 
     return w / 2 if w != 0 else 0
@@ -60,5 +76,5 @@ for _ in range(T):
     W, N = int(input()), int(input())
     Sensors = [[0, 0, 0]] + [list(map(int, input().split())) for _ in range(N)]
     # idx 0 왼벽, idx N+1 오벽
-    S = [-1] * 101010
+    S = [-1] * (N + 2)
     print(jud())
