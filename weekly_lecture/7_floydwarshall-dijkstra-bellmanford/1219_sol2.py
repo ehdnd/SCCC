3d55099f6df86e44b15735debcc895f7c937e268
ehdnd_mac <taeungjang@naver.com>
2024-12-02 00:18:09 +0900

    COR: [1219] (P5) 오민식의 고민 - sol1) 간선 새로 모델링
    사이클이 결과 영향 있는지 확인
    벨만 - 포드

--- weekly_lecture/7_floydwarshall-dijkstra-bellmanford/1219_sol2.py
+++ weekly_lecture/7_floydwarshall-dijkstra-bellmanford/1219_sol2.py
@@ -7,51 +7,56 @@ inf = int(1e9)
 
 def bfs(v):
     q = deque([v])
-    V = [0] * N
+    V = [0] * (2 * N)
     while q:
         x = q.popleft()
         for nx, _ in G[x]:
             if not V[nx]:
                 V[nx] = 1
                 q.append(nx)
-    return V[ed]
+    return V[N + ed]
 
 
 N, st, ed, M = map(int, input().split())
-G = [[] for _ in range(N)]
-for _ in range(M):
-    a, b, cst = map(int, input().split())
-    G[a].append([b, cst])
+lines = [list(map(int, input().split())) for _ in range(M)]
 C = list(map(int, input().split()))
 
+G = [[] for _ in range(2 * N)]
+
+# sol2) 정점 분할
+for i in range(N):
+    i_in = i
+    i_out = N + i
+    G[i_in].append([i_out, -C[i]])
+
+for a, b, cst in lines:
+    a_out = N + a
+    b_in = b
+    G[a_out].append([b_in, cst])
+
 # 목적지 도달 가능 확인
-P = [0] * N
-for x in range(N):
+P = [0] * (2 * N)
+for x in range(2 * N):
     P[x] = bfs(x)
 
-# sol1) 간선 모델링
-for start in range(N):
-    for i in range(len(G[start])):
-        end = G[start][i][0]
-        G[start][i][1] -= C[end]
-
-dist = [inf] * N
-dist[st] = -C[st]
+dist = [inf] * (2 * N)
+dist[st] = 0
 
 isCycle = False
-for i in range(N):
-    for x in range(N):
+for i in range(2 * N):
+    for x in range(2 * N):
         if dist[x] == inf:
             continue
         for nx, w in G[x]:
             if dist[nx] > dist[x] + w:
                 dist[nx] = dist[x] + w
-                if i == N - 1 and P[nx]:
+                if i == 2 * N - 1 and P[nx]:
                     isCycle = True
 
-if dist[ed] == inf:
+
+if dist[N + ed] == inf:
     print("gg")
 elif isCycle:
     print("Gee")
 else:
-    print(-dist[ed])
+    print(-dist[N + ed])
