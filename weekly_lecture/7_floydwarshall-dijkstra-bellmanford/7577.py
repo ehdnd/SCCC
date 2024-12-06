3b5f9d8f1f1032819ee94bd8ea6f4982fd4f94a8
ehdnd_mac <taeungjang@naver.com>
2024-12-06 13:05:47 +0900

    COR: [7577] (D5) 탐사 - REF 필요 - SPFA 알고리즘을 채용하면 필요한 계산만 가능..

--- weekly_lecture/7_floydwarshall-dijkstra-bellmanford/7577.py
+++ weekly_lecture/7_floydwarshall-dijkstra-bellmanford/7577.py
@@ -1,41 +1,50 @@
 import sys
-from typing import Optional
+from typing import Optional, List, Tuple
 
 input = lambda: sys.stdin.readline().rstrip()
 INF = int(1e9)
 
 
-def bellman_ford() -> Optional[list[int]]:
-    dist = [INF] * (K + 2)
-    dist[K + 1] = 0
+def creat_graph(K: int, N: int) -> List[List[Tuple[int, int]]]:
+    G = [[] for _ in range(K + 2)]
 
-    for i in range(K + 2):
-        isChanged = False
-        for x in range(0, K + 2):
-            if dist[x] == INF:
-                continue
+    for _ in range(N):
+        x, y, r = map(int, input().split())
+        G[x - 1].append([y, r])
+        G[y].append([x - 1, -r])
 
-            for nx, w in G[x]:
-                if dist[nx] > dist[x] + w:
-                    dist[nx] = dist[x] + w
-                    if i == K + 1:
-                        isChanged = True
-    if isChanged:
-        return None
-    return dist
-
-
-def jud() -> None:
-    # 모든 노드에 대한 새로운 시작점을 K+1 에 만들어주자
     for i in range(0, K + 1):
         G[K + 1].append([i, 0])
 
-    # 0 <= (1, 2] <= 1
     for i in range(1, K + 1):
         G[i - 1].append([i, 1])
         G[i].append([i - 1, 0])
 
-    res = bellman_ford()
+    return G
+
+
+def bellman_ford(G: List[List[Tuple[int, int]]], K: int) -> Optional[List[int]]:
+    dist = [INF] * (K + 2)
+    dist[K + 1] = 0
+
+    for i in range(K + 2):
+        for x in range(0, K + 2):
+            if dist[x] == INF:
+                continue
+            for nx, w in G[x]:
+                if dist[nx] > dist[x] + w:
+                    dist[nx] = dist[x] + w
+                    if i == K + 1:
+                        return None
+
+    return dist
+
+
+def solve() -> None:
+    K, N = map(int, input().split())
+    G = creat_graph(K, N)
+
+    res = bellman_ford(G, K)
     if not res:
         print("NONE")
         return
@@ -48,17 +57,5 @@ def jud() -> None:
     return
 
 
-# 단순연립부등식을 벨만포드로 모델링
-# difference constraints and shortest paths
-# 부등식을 그래프로 표현만 하면 알아서 알맞은 해를 찾아준다
 if __name__ == "__main__":
-    K, N = map(int, input().split())
-    G = [[] for _ in range(K + 2)]
-    for _ in range(N):
-        x, y, r = map(int, input().split())
-        # (x-1, y] == r 은
-        # (x-1, y] <= r / (x-1, y] >= r 로 거리 제약 조건
-        G[x - 1].append([y, r])
-        G[y].append([x - 1, -r])
-
-    jud()
+    solve()
