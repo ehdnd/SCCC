8a524791f15a0bf358300ee5379be74b5499d156
ehdnd_mac <taeungjang@naver.com>
2024-12-15 14:26:30 +0900

    COR: [1261] (G4) 알고스팟 - 0-1너비우선탐색

--- BOJ_extra/graph/1261.py
+++ BOJ_extra/graph/1261.py
@@ -4,26 +4,33 @@ from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
 MIS = lambda: map(int, input().split())
 II = lambda: int(input())
+INF = int(1e9)
+
+
+def chk(i: int, K: int):
+    return 0 <= i <= K
 
 
 def bfs(a: int, b: int):
     q = deque()
     q.append([a, b])
-    L[a][b] = 1
-
+    L[a][b] = 0
     while q:
         x, y = q.popleft()
         if x == N - 1 and y == M - 1:
-            return L[x][y] - 1
+            return L[x][y]
 
         dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx > N - 1 or ny < 0 or ny > M - 1 or L[nx][ny]:
-                continue
-            l = G[nx][ny]
-            L[nx][ny] = L[x][y] + l
-            q.appendleft([nx, ny]) if l == 0 else q.append([nx, ny])
+            if chk(nx, N - 1) and chk(ny, M - 1):
+                lv_diff = G[nx][ny]
+                if L[nx][ny] > L[x][y] + lv_diff:
+                    L[nx][ny] = L[x][y] + lv_diff
+                    if lv_diff == 1:
+                        q.append([nx, ny])
+                    else:
+                        q.appendleft([nx, ny])
 
     return
 
@@ -36,5 +43,5 @@ def solve() -> None:
 if __name__ == "__main__":
     M, N = MIS()
     G = [list(map(int, input())) for _ in range(N)]
-    L = [[0] * M for _ in range(N)]
+    L = [[INF] * M for _ in range(N)]
     solve()
