06481e02307fc924427a9656af19ce7c6228766d
ehdnd_mac <taeungjang@naver.com>
2024-12-15 14:08:49 +0900

    COR: [1261] (G4) 알고스팟 - 0-1 너비우선탐색

--- BOJ_extra/graph/1261.py
+++ BOJ_extra/graph/1261.py
@@ -4,28 +4,24 @@ from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
 MIS = lambda: map(int, input().split())
 II = lambda: int(input())
-INF = int(1e9)
 
 
 def bfs(a: int, b: int):
     q = deque()
     q.append([a, b])
-    L[a][b] = 0
+    L[a][b] = 1
 
     while q:
         x, y = q.popleft()
         if x == N - 1 and y == M - 1:
-            return L[x][y]
+            return L[x][y] - 1
 
         dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx > N - 1 or ny < 0 or ny > M - 1:
+            if nx < 0 or nx > N - 1 or ny < 0 or ny > M - 1 or L[nx][ny]:
                 continue
-
             l = G[nx][ny]
-            if L[nx][ny] <= L[x][y] + l:
-                continue
             L[nx][ny] = L[x][y] + l
             q.appendleft([nx, ny]) if l == 0 else q.append([nx, ny])
 
@@ -40,5 +36,5 @@ def solve() -> None:
 if __name__ == "__main__":
     M, N = MIS()
     G = [list(map(int, input())) for _ in range(N)]
-    L = [[INF] * M for _ in range(N)]
+    L = [[0] * M for _ in range(N)]
     solve()
