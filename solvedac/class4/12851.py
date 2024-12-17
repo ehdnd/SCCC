ff34f9daac14c487028f67dacf80b3742410fbe6
ehdnd_desktop <taeungjang@naver.com>
2024-12-18 00:38:01 +0900

    COR: [12851] (G4) 숨바꼭질 2 - bfs 거꾸리해봤어요..

--- solvedac/class4/12851.py
+++ solvedac/class4/12851.py
@@ -11,33 +11,36 @@ def chk(i: int) -> bool:
     return 0 <= i <= MAX
 
 
-def bfs(V: list[int], N: int, K: int) -> list[list[int]]:
-    G = [[] for _ in range(MAX + 1)]
+def bfs(V: list[int], N: int, K: int) -> None:
     q = deque([N])
     V[N] = 1
     while q:
         x = q.popleft()
+        if x == K:
+            return
         for nx in [x - 1, x + 1, 2 * x]:
-            if chk(nx):
-                if not V[nx] or V[nx] == V[x] + 1:
-                    if not V[nx]:
-                        q.append(nx)
-                    V[nx] = V[x] + 1
-                    G[nx].append(x)
+            if chk(nx) and not V[nx]:
+                V[nx] = V[x] + 1
+                q.append(nx)
 
-    return G
+    return
 
 
-def find_path(G: list[int], N: int, K: int) -> int:
+def find_path(V: list[int], N: int, K: int) -> int:
     P = [0] * (MAX + 1)
     q = deque([K])
     P[K] = 1
     while q:
         x = q.popleft()
-        for px in G[x]:
-            if not P[px]:
-                q.append(px)
-            P[px] += P[x]
+        if x == N:
+            break
+
+        pxs = [x + 1, x - 1, x // 2] if x % 2 == 0 else [x + 1, x - 1]
+        for px in pxs:
+            if chk(px) and V[px] + 1 == V[x]:
+                if not P[px]:
+                    q.append(px)
+                P[px] += P[x]
 
     return P[N]
 
@@ -45,9 +48,9 @@ def find_path(G: list[int], N: int, K: int) -> int:
 def solve() -> None:
     N, K = MIS()
     V = [0] * (MAX + 1)
-    G = bfs(V, N, K)
+    bfs(V, N, K)
     print(V[K] - 1)
-    print(find_path(G, N, K))
+    print(find_path(V, N, K))
     return
 
 
