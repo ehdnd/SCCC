dcda9cd46063a6a6e13aae93c8fbc2a82f9a0a6c
ehdnd_mac <taeungjang@naver.com>
2024-12-30 19:10:35 +0900

    COR: [11723] (G5) 집합 - 비트마스킹을 연습

--- weekly_lecture/bit-masking/11723.cpp
+++ weekly_lecture/bit-masking/11723.cpp
@@ -6,16 +6,10 @@ using p = pair<ll, ll>;
 int M, S;
 string query;
 
-void add(int x) { S |= (1 << (x - 1)); }
-void remove(int x) { S &= ~(1 << (x - 1)); }
-// bool.. 0이면 false 아닌 모든 값은 true 반환
-bool check(int x) { return S & (1 << (x - 1)); }
-void toggle(int x) {
-  if (check(x))
-    remove(x);
-  else
-    add(x);
-}
+void add(int x) { S |= (1 << x); }
+void remove(int x) { S &= ~(1 << x); }
+bool check(int x) { return (S & (1 << x)) != 0; }
+void toggle(int x) { S ^= (1 << x); }
 void all() { S = (1 << 20) - 1; }
 void empty() { S = 0; }
 
@@ -26,16 +20,16 @@ void Solve() {
     int x;
     if (query == "add") {
       cin >> x;
-      add(x);
+      add(x - 1);
     } else if (query == "remove") {
       cin >> x;
-      remove(x);
+      remove(x - 1);
     } else if (query == "check") {
       cin >> x;
-      cout << check(x) << "\n";
+      cout << check(x - 1) << "\n";
     } else if (query == "toggle") {
       cin >> x;
-      toggle(x);
+      toggle(x - 1);
     } else if (query == "all") {
       all();
     } else {
