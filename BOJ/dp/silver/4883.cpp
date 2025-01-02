dce69e796d617033d5f4cde1f4c8fdb8cc24d2a1
ehdnd_mac <taeungjang@naver.com>
2025-01-02 15:01:49 +0900

    COR: [4883] (S1) 삼각 그래프 - 초기화 잘하자

--- BOJ/dp/silver/4883.cpp
+++ BOJ/dp/silver/4883.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using p = pair<ll, ll>;
 
 const int MAX = 101010;
+const int INF = 1e9;
 int N, cnt;
 int G[MAX][3];
-int dp[MAX];
+int dp[MAX][3];
 
 void Solve() {
   for (int i = 0; i < N; i++)
     for (int j = 0; j < 3; j++) cin >> G[i][j];
 
-  dp[0] = G[0][1];
-  dp[1] = G[0][1] + G[1][1];
+  dp[0][0] = INF;  // 초기화좀 잘 해주자..
+  dp[0][1] = G[0][1];
+  dp[0][2] = G[0][2] + G[0][1];
 
-  for (int i = 2; i < N; i++) {
-    dp[i] = G[i][1] +
-            min({dp[i - 2] + G[i - 1][0], dp[i - 2] + G[i - 1][2], dp[i - 1]});
+  for (int i = 1; i < N; i++) {
+    dp[i][0] = G[i][0] + min(dp[i - 1][0], dp[i - 1][1]);
+    dp[i][1] =
+        G[i][1] + min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2], dp[i][0]});
+    dp[i][2] = G[i][2] + min({dp[i - 1][1], dp[i - 1][2], dp[i][1]});
   }
-  cout << ++cnt << ". " << dp[N - 1] << "\n";
+
+  cout << ++cnt << ". " << dp[N - 1][1] << "\n";
 }
 
 int main() {
