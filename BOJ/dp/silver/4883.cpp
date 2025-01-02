2a970863e726a6d6e1eac7c83844d5e79ed60993
ehdnd_mac <taeungjang@naver.com>
2025-01-02 15:02:25 +0900

    REF: [4883] (S1) 삼각 그래프 - 슬라이딩 윈도우

--- BOJ/dp/silver/4883.cpp
+++ BOJ/dp/silver/4883.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-const int MAX = 101010;
 const int INF = 1e9;
-int N, cnt;
-int G[MAX][3];
-int dp[MAX][3];
+int N;
+int cnt, a, b, c;
+int prev_dp[3], curr_dp[3];
 
 void Solve() {
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < 3; j++) cin >> G[i][j];
-
-  dp[0][0] = INF;  // 초기화좀 잘 해주자..
-  dp[0][1] = G[0][1];
-  dp[0][2] = G[0][2] + G[0][1];
+  cin >> a >> b >> c;
+  prev_dp[0] = INF;
+  prev_dp[1] = b;
+  prev_dp[2] = c + prev_dp[1];
 
   for (int i = 1; i < N; i++) {
-    dp[i][0] = G[i][0] + min(dp[i - 1][0], dp[i - 1][1]);
-    dp[i][1] =
-        G[i][1] + min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2], dp[i][0]});
-    dp[i][2] = G[i][2] + min({dp[i - 1][1], dp[i - 1][2], dp[i][1]});
+    cin >> a >> b >> c;
+    curr_dp[0] = a + min({prev_dp[0], prev_dp[1]});
+    curr_dp[1] = b + min({prev_dp[0], prev_dp[1], prev_dp[2], curr_dp[0]});
+    curr_dp[2] = c + min({prev_dp[1], prev_dp[2], curr_dp[1]});
+
+    swap(prev_dp, curr_dp);
   }
 
-  cout << ++cnt << ". " << dp[N - 1][1] << "\n";
+  cout << ++cnt << ". " << prev_dp[1] << "\n";
 }
 
 int main() {
