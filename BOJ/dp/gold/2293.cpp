3594ff1f90d98cb6e1ecc8dcf8fecd8ca139a2f1
ehdnd_desktop <taeungjang@naver.com>
2025-01-08 17:09:41 +0900

    REF: [2293] (G4) 동전 1 - 1차원 DP가 정해인가보다

--- BOJ/dp/gold/2293.cpp
+++ BOJ/dp/gold/2293.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int N, K;
 vector<int> P;
-int prev_dp[10101];
-int curr_dp[10101];
+vector<ll> dp;
 
 void Solve() {
   cin >> N >> K;
+
   P.resize(N);
   for (auto& p : P) cin >> p;
 
-  // coin(n,k) 가 1~n번째 동전만 사용해서 k원을 나타내는 가짓수라고 하자.
-  curr_dp[0] = prev_dp[0] = 1;
+  dp.resize(K + 1);
+  dp[0] = 1;
 
-  for (const auto& p : P) {
-    for (int k = 1; k <= K; k++) {
-      if (p > k) {
-        curr_dp[k] = prev_dp[k];
-      } else {
-        curr_dp[k] = curr_dp[k - p] + prev_dp[k];
-      }
-    }
+  for (const auto& p : P)
+    for (int j = p; j <= K; j++) dp[j] += dp[j - p];
 
-    swap(curr_dp, prev_dp);
-  }
-
-  cout << prev_dp[K];
+  cout << dp[K];
 }
 
 int main() {
