f7672eea77914468f6266634319f73f3943d490a
ehdnd_mac <taeungjang@naver.com>
2025-03-21 14:13:38 +0900

    FAIL: [32870] (P5) 수열과 쿼리 HY - 전처리 +  버킷별로 계산하자

--- weekly_practice/25SS/week3/32870.cpp
+++ weekly_practice/25SS/week3/32870.cpp
@@ -1,62 +1,54 @@
 #include <bits/stdc++.h>
 #define all(v) v.begin(), v.end()
 using namespace std;
+using pii = pair<int, int>;
 
 const int INF = 1e9;
+const int M = 3e5;
 
 int N, Q;
 vector<int> nums;
+vector<pii> ans;
+
+// 전처리르 하세요.
 
 void Solve() {
   cin >> N >> Q;
   nums.resize(N, 0);
+  ans.resize(M + 1, {0, 0});
   for (int& x : nums) cin >> x;
 
   sort(all(nums));
 
+  const int MAX_NUM = nums.back();
+
+  for (int m = 1; m <= M; ++m) {
+    int min_a = INF;
+    int max_a = 0;
+
+    for (int k = 0; k * m <= MAX_NUM; ++k) {
+      // 구간 [lo, hi]
+      int lo = k * m;
+      int hi = min(m * (k + 1) - 1, MAX_NUM);
+
+      auto it1 = lower_bound(all(nums), lo);
+      if (it1 == nums.end()) break;
+      if (hi < *it1) continue;
+
+      auto it2 = upper_bound(all(nums), hi);
+      it2--;
+
+      min_a = min(*it1 % m, min_a);
+      max_a = max(*it2 % m, max_a);
+    }
+
+    ans[m] = {min_a, max_a};
+  }
+
   while (Q--) {
     int q;
     cin >> q;
-
-    if (N == 1) {
-      cout << nums[0] % q << "\n";
-      continue;
-    }
-
-    int max_a = nums[0] % q;
-    int min_a = nums[0] % q;
-    int qm = q;
-
-    for (int i = 2;; ++i) {
-      auto it = lower_bound(all(nums), qm);
-
-      if (it == nums.begin()) {
-        max_a = max({max_a, *it % q, *(it + 1) % q});
-        min_a = min({min_a, *it % q, *(it + 1) % q});
-        qm = q * i;
-        continue;
-      }
-
-      if (it == nums.end()) {
-        max_a = max({max_a, *(it - 1) % q});
-        min_a = min({min_a, *(it - 1) % q});
-        break;
-      }
-
-      if (it == nums.end() - 1) {
-        max_a = max({max_a, *it % q, *(it - 1) % q});
-        min_a = min({min_a, *it % q, *(it - 1) % q});
-        qm = q * i;
-        continue;
-      }
-
-      max_a = max({max_a, *(it + 1) % q, *(it - 1) % q, *it % q});
-      min_a = min({min_a, *(it + 1) % q, *(it - 1) % q, *it % q});
-
-      qm = q * i;
-    }
-
-    cout << min_a << " " << max_a << "\n";
+    cout << ans[q].first << " " << ans[q].second << "\n";
   }
 }
 
